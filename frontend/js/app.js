4bf530a228a7e9fce315b547251aaaa16da11036
lyndalebdjiri <94696703+lyndalebdjiri@users.noreply.github.com>
2026-02-28 04:18:39 +0100

    re changing the URL render

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -5,7 +5,7 @@ async function analyzePhoto() {
     formData.append('survey', JSON.stringify(userData.survey));
     formData.append('photo', userData.photo);
 
-    const response = await fetch('https://dermascan-backend-5u4m.onrender.com/analyze', {
+    const response = await fetch('https://dermascan-33ua.onrender.com/analyze', {
       method: 'POST',
       body: formData
     });
